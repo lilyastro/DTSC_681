f7ecff928f4699d6edc4feff9905565f69aff9ca
Lily Loyer <lily.loyer@sproutward.com>
2024-06-30 21:47:24 -0400

    fix to resume

--- app/docs/resume.py
+++ app/docs/resume.py
@@ -6,13 +6,15 @@ def display_resume_pdf(file):
     """Function to Display Resume (PDF)
     Args: None
     Returns: Obj: Streamlit Markdown display"""
-    
-    # Open File
-    with open(file, "rb") as f:
-        pdf = base64.b64encode(f.read()).decode('utf-8')
 
-    # Embedding PDF in HTML
-    pdf_fixed = F'<iframe src="data:application/pdf;base64,{pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    # did not work on streamlit cloud
+    
+    # # Open File
+    # with open(file, "rb") as f:
+    #     pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # # Embedding PDF in HTML
+    # pdf_fixed = F'<iframe src="data:application/pdf;base64,{pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     # Displaying File
-    return st.markdown(pdf_fixed, unsafe_allow_html=True)
+    return st.image('app/docs/Resume.jpg', caption='Resume', use_column_width=True)
 
